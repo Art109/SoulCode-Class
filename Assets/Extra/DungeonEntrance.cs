2d4f13cb10ad2e7c441056f439dba15d91de0ac6
Arthur Aguiar <arthur.espinaguiar@gmail.com>
2025-03-21 17:12:52 -0300

    Last task in 2D

---
+++ Assets/Extra/DungeonEntrance.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonEntrance : MonoBehaviour
+{
+    [SerializeField] Transform teleportPosition;
+    bool inDungeon = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inDungeon)
+        {
+
+        }
+    }
+}
